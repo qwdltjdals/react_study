a3d6d431816b29b986195f2a8f51e35cdb576e4e
qwdltjdals <iu_love@kakao.com>
2024-07-26 12:40:30 +0900

    백업

--- async_study/src/pages/basic/ComputerPage.jsx
+++ async_study/src/pages/basic/ComputerPage.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
 import axios from 'axios';
+import ReactModal from 'react-modal';
+ReactModal.setAppElement("#root");
 
 const layout = css`
     box-sizing: border-box;
@@ -10,7 +12,26 @@ const layout = css`
 `;
 
 function Computer(props) {
+
+    const [isModalOpen, setModalOpen] = useState(false);
+
+    const [computerDetail, setComputerDetail] = useState({
+        computerId: "",
+        company: "",
+        cpu: "",
+        ram: "",
+        ssd: "",
+    });
+
     const [registerComputer, setRegisterComputer] = useState({
+        company: "",
+        cpu: "",
+        ram: "",
+        ssd: "",
+    });
+
+    const [updateComputer, setUpdateComputer] = useState({
+        computerId: "",
         company: "",
         cpu: "",
         ram: "",
@@ -21,7 +42,7 @@ function Computer(props) {
         company: "",
         cpu: "",
     });
-    
+
 
     const [computerList, setComputerList] = useState([]);
 
@@ -31,7 +52,7 @@ function Computer(props) {
                 params // 원래 : params : params 인데, 키값과 벨류가 같으니까 이렇게 써도됨
             });
             setComputerList(response.data);
-        } catch(e) {
+        } catch (e) {
             console.error(e)
         }
     }
@@ -40,7 +61,7 @@ function Computer(props) {
         setparams(p => {
             return {
                 ...p,
-                [e.target.name] : e.target.value
+                [e.target.name]: e.target.value
             }
         });
     }
@@ -55,13 +76,39 @@ function Computer(props) {
     }
 
     const handleSearchClick = () => {
-        
+
         requsetComputerList(); // 클릭이 되면 리스트 줌
 
         setparams({ // 빈값으로 바꿔줌
             company: "",
             cpu: "",
-        })
+        });
+    }
+
+    const handleSelectComputerClick = async (computerId) => {
+        const data = await requestGetComputer(computerId);
+        if (!data) {
+            setComputerDetail({
+                computerId: "",
+                company: "",
+                cpu: "",
+                ram: "",
+                ssd: "",
+            });
+            return;
+        }
+        setComputerDetail(data);
+    };
+
+    const requestGetComputer = async (computerId) => {
+        let responseData = null;
+        try {
+            const response = await axios.get(`http://localhost:8080/api/v1/computer/${computerId}`); // ${}사용하기 위해 `` 썼음
+            responseData = response.data;
+        } catch (e) {
+            console.error(e);
+        }
+        return responseData;
     }
 
     const handleRegisterSubmitClick = async () => {
@@ -76,8 +123,99 @@ function Computer(props) {
             alert("등록실패!!");
         }
     }
+
+    const handleDeleteComputerClick = async (computerId) => {
+        if (window.confirm("정말 삭제하시겠습니까?")) {
+            await requestDeleteComputer(computerId);
+            await requsetComputerList();
+            alert("삭제 완료");
+        }
+    }
+
+    const requestDeleteComputer = async (computerId) => {
+        let responseData = null;
+        try {
+            const response = await axios.delete(`http://localhost:8080/api/v1/computer/${computerId}`);
+            responseData = response.data
+        } catch (e) {
+            console.error(e);
+        }
+        return responseData
+    }
+
+    const closeModal = () => {
+        setModalOpen(false);
+        setUpdateComputer({
+            computerId: "",
+            company: "",
+            cpu: "",
+            ram: "",
+            ssd: "",
+        });
+    }
+
+    const handleUpdateComputerClick = async (computerId) => {
+        setModalOpen(true);
+        const data = await requestGetComputer(computerId);
+        setUpdateComputer(data);
+    }
+
+    const handleUpdateSubmitClick = async () => {
+        await requestUpdateComputer();
+        await requsetComputerList();
+        closeModal();
+    }
+
+    const requestUpdateComputer = async () => {
+        let responseData = null;
+        try {
+            const response = await axios.put(`http://localhost:8080/api/v1/computer/${updateComputer.computerId}`, updateComputer) // 업데이트컴퓨터 안에 정보가 들어있을 것.
+            responseData = response.data;
+        } catch (e) {
+            console.error(e)
+        }
+        return responseData;
+    }
+
+    const handleUpdateInputChange = (e) => {
+        setUpdateComputer(uc => {
+            return {
+                ...uc,
+                [e.target.name]: e.target.value
+            }
+        });
+    }
+
     return (
         <div>
+            <ReactModal
+                style={{
+                    content: {
+                        boxSizing: `boder-box`,
+                        transform: `translate(-50%, -50%)`,
+                        top: `50%`,
+                        left: `50%`,
+                        width: `400px`,
+                        height: `400px`,
+                        BackgroundColor: `#fafafa`
+                    }
+                }}
+                isOpen={isModalOpen}
+                onRequestClose={closeModal}
+            >
+                <div css={css`display`}>
+                    <h2>컴퓨터 정보 수정</h2>
+                    <p><input type="text" name='computerId' onChange={handleUpdateInputChange} value={updateComputer.computerId} disabled={true} /></p>
+                    <p><input type="text" name='company' placeholder='제조사' onChange={handleUpdateInputChange} value={updateComputer.company} /></p>
+                    <p><input type="text" name='cpu' placeholder='CPU' onChange={handleUpdateInputChange} value={updateComputer.cpu} /></p>
+                    <p><input type="text" name='ram' placeholder='RAM' onChange={handleUpdateInputChange} value={updateComputer.ram} /></p>
+                    <p><input type="text" name='ssd' placeholder='SSD' onChange={handleUpdateInputChange} value={updateComputer.ssd} /></p>
+                    <div>
+                        <button onClick={handleUpdateSubmitClick}>확인</button>
+                        <button onClick={() => closeModal()}>취소</button>
+                    </div>
+                </div>
+            </ ReactModal>
             <div css={layout}>
                 <h2>목록</h2>
                 <p>
@@ -97,13 +235,13 @@ function Computer(props) {
                     </thead>
                     <tbody>
                         {
-                            computerList.map(computer => 
+                            computerList.map(computer =>
                                 <tr key={computer.computerId}>
-                                    <td><button>선택</button></td>
+                                    <td><button onClick={() => handleSelectComputerClick(computer.computerId)}>선택</button></td>
                                     <td>{computer.computerId}</td>
                                     <td>{computer.company}</td>
-                                    <td><button>수정</button></td>
-                                    <td><button>삭제</button></td>
+                                    <td><button onClick={() => handleUpdateComputerClick(computer.computerId)}>수정</button></td>
+                                    <td><button onClick={() => handleDeleteComputerClick(computer.computerId)}>삭제</button></td>
                                 </tr>
                             )
                         }
@@ -112,7 +250,13 @@ function Computer(props) {
             </div>
             <div css={layout}>
                 <h2>세부정보</h2>
-
+                <ul>
+                    <li>ID : {computerDetail.computerId}</li>
+                    <li>제조사 : {computerDetail.company}</li>
+                    <li>CPU : {computerDetail.cpu}</li>
+                    <li>RAM : {computerDetail.ram}</li>
+                    <li>SSD : {computerDetail.ssd}</li>
+                </ul>
             </div>
             <div css={layout}>
                 <h2>등록</h2>
@@ -151,10 +295,6 @@ function Computer(props) {
                 <p>
                     <button onClick={handleRegisterSubmitClick}>등록하기</button>
                 </p>
-            </div>
-            <div css={layout}>
-                <h2>수정</h2>
-
             </div>
         </div>
     );
